b8e88dca5efa50c14a25c8cd200f871a188ad004
Ozren Kosi <ozrenkosi@users.noreply.github.com>
2018-12-31 14:15:10 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,13 +5,24 @@ let birdImage;
 let resetButton;
 let instructions;
 let score;
+let flySound;
+let hitSound;
+let pointSound;
+let swooshSound;
 let birdIsAlive = true;
 let waitingForStart = true;
 
-function setup() {
-  createCanvas(windowHeight/2, windowHeight);
+function preload() {
   bgImage = loadImage("assets/bg.jpg");
   birdImage = loadImage("assets/bird.png");
+  flySound = loadSound("assets/sfx_wing.mp3");
+  hitSound = loadSound("assets/sfx_hit.mp3");
+  pointSound = loadSound("assets/sfx_point.mp3");
+  swooshSound = loadSound("assets/sfx_swooshing.mp3");
+}
+
+function setup() {
+  createCanvas(windowHeight/2, windowHeight);
 
   bird = new Bird();
   resetButton = new ResetButton();
@@ -35,11 +46,12 @@ function draw() {
       pipes[i].updatePosition();
       pipes[i].show();
 
-      if (pipes[i].hits(bird)) {
+      if (pipes[i].hits(bird) || bird.touchesFloor()) {
         gameOver();
       }
 
       if (pipes[i].offScreen()) {
+        // Deletes pipe
         pipes.shift();
       }
     }
@@ -55,6 +67,7 @@ function draw() {
 function keyPressed() {
   if (this.key = ' ' && birdIsAlive === true) {
     bird.jump();
+    flySound.play();
   }
 
   if (this.key = ' ' && waitingForStart === true) {
@@ -63,6 +76,11 @@ function keyPressed() {
 }
 
 function gameOver() {
+  if(birdIsAlive === true) {
+    hitSound.play();
+    swooshSound.play();
+  }
+
   for (let i = pipes.length-1; i >= 0; i--) {
     pipes[i].speed = 0;
   }
@@ -71,8 +89,8 @@ function gameOver() {
   resetButton.show();
 }
 
-// Reset Game
 function mousePressed() {
+  // Reset Game
   if (resetButton.clickedOn() && birdIsAlive === false) {
     pipes = [];
     bird = new Bird();
