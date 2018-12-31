cdc3a7b8ab3088896f1e48c226ef29d9fdd421f1
Ozren Kosi <ozrenkosi@users.noreply.github.com>
2018-12-31 12:56:54 +0100

    Update Score.js

--- components/Score.js
+++ components/Score.js
@@ -5,7 +5,7 @@ class Score {
 
   update() {
     for (let i = pipes.length-1; i >= 0; i--) {
-      if (pipes[i].x < bird.x-bird.birdWidth/2 && pipes[i].x > bird.x-bird.birdWidth/2-3) {
+      if (pipes[i].x < bird.x-bird.birdWidth/2 && pipes[i].x > bird.x-bird.birdWidth/2-3 && birdIsAlive === true) {
         this.text = this.text + 1;
       }
     }
